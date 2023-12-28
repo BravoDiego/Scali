d6d2aecf036b355cdae765659df203d9b7aead77
BravoDiego <101215860+BravoDiego@users.noreply.github.com>
2023-12-28 22:31:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export default {
-    token: "MTE4OTMwMTcyMjQxMDE0MzgwNQ.G8UoRX.v08c2_ELHnDlp8ngZPIam3K32PKz9EzYM2Zk2g",
+    token: "token",
     code: "EbKLqq4UdWT8grtS3h5G5BHj3nljLKgW5EAIkl6x",
 };
