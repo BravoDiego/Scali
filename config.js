bf7432540f8b4cdd6e78f77b7aa439a7385d7313
BravoDiego <101215860+BravoDiego@users.noreply.github.com>
2023-12-29 14:29:28 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export default {
-    token: "MTE4OTMwMTcyMjQxMDE0MzgwNQ.GrRwKv.dgF3IeHCzg9NvyWwzFrheosbAqipCe_IEJIOmo",
+    token: "token",
     code: "EbKLqq4UdWT8grtS3h5G5BHj3nljLKgW5EAIkl6x",
 };
